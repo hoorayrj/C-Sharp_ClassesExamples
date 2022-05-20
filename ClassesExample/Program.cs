cfdeeec4024c46f3a48fa680751e67d4f2709ddb
Ray <c2rjay@gmail.com>
2022-05-20 09:53:06 -0400

    Add project files.

---
+++ ClassesExample/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Person
+{
+    // Class variable
+    public string Name;
+
+    // Class Method
+    public void Introduce(string to)
+    {
+        // String template used here. 
+        Console.WriteLine("Hi {0}, I am {1}", to, Name);
+    }
+
+    // Parse Method within the class to handle the string. Make it static so that it is accessible for the class
+    // and not an object
+    public static Person Parse (string str)
+    {
+        var parPerson = new Person();
+        parPerson.Name = str;
+
+        return parPerson;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+        var person = Person.Parse("John");
+
+        person.Introduce("Ray");
+    }
+}
+
+
+
+
+
+
